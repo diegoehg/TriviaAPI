cadff0e8bdda72ed26031d06eff60b818a96f637
Diego Hdez <diegoehg@gmail.com>
2020-12-30 16:22:59 -0600

    Changing database path configuration

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -14,8 +14,10 @@ class TriviaTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
-        self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_dialect = "postgresql"
+        self.database_location = "localhost:5432"
+        self.database_name = "trivia_test.db"
+        self.database_path = f"{self.database_dialect}://{self.database_location}/{self.database_name}"
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
