cd73bc000c4a04f09ec175d337fadb58a99f45b5
Diego Hdez <diegoehg@gmail.com>
2021-01-05 23:41:00 -0600

    It is replaced PostgreSQL restart

--- backend/setup_db.sh
+++ backend/setup_db.sh
@@ -1,4 +1,4 @@
-sudo systemctl start postgresql@12-main
+sudo -u postgres bash -c "pg_ctlcluster 12 main restart"
 sudo -u postgres bash -c "psql < setup.sql"
 sudo -u postgres bash -c "psql trivia < trivia.psql"
 sudo -u postgres bash -c "psql trivia_test < trivia.psql"
